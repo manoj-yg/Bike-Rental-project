c5e3078b51d675dfa3054160ee332f68bb6b65d5
manoj-yg <manojgangadhar20@gmail.com>
2024-10-24 02:38:48 +0530

    changed all files of the project

---
+++ JS/main.js
@@ -0,0 +1,65 @@
+const bikeData = [
+    {
+      name: "BMW R1300GS",
+      type: "ADVENTURE",
+      price: 17,
+      image: "images/bike-1.png",
+      tag: "Free Cancellation",
+    },
+    {
+      name: "BMW R1300GS",
+      type: "ADVENTURE",
+      price: 17,
+      image: "images/bike-2.png",
+      tag: "Free Cancellation",
+    },
+    {
+      name: "BMW R1300GS",
+      type: "ADVENTURE",
+      price: 17,
+      image: "images/bike-3.png",
+      tag: "Free Cancellation",
+    },
+    {
+      name: "BMW R1300GS",
+      type: "ADVENTURE",
+      price: 17,
+      image: "images/bike-4.png",
+      tag: "Free Cancellation",
+    },
+    {
+      name: "BMW R1300GS",
+      type: "ADVENTURE",
+      price: 17,
+      image: "images/bike-5.png",
+      tag: "Free Cancellation",
+    },
+    {
+      name: "BMW R1300GS",
+      type: "ADVENTURE",
+      price: 17,
+      image: "images/bike-6.png",
+      tag: "Free Cancellation",
+    },
+    {
+      name: "BMW R1300GS",
+      type: "ADVENTURE",
+      price: 17,
+      image: "images/bike-7.png",
+      tag: "Free Cancellation",
+    },
+    {
+      name: "BMW R1300GS",
+      type: "ADVENTURE",
+      price: 17,
+      image: "images/bike-8.png",
+      tag: "Free Cancellation",
+    },
+  ];
+
+  //menu
+
+  let menu=document.querySelector('.menu-icon');
+  menu.onclick=()=>{
+    menu.classList.toggle("move")
+  };
